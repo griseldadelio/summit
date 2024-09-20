5d343347e3d9c6ff2c5fc84ae287976daa089951
griselda de lio <griseldadelio@hotmail.com>
2024-09-20 17:56:27 -0300

    ajuste

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Hero from '../components/Hero';
 import Caresection from '../components/CareSection';
-import OurBusiness from '../components/Ourbusiness';
+import OurBusiness from '../components/OurBusiness';
 import ContactSection from '../components/ContactSection';
 
 export default function Home() {
